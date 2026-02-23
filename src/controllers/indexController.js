4c8c55ebada6e4b5b24f4568d06f42bae50002fb
RR <rrn091301@gmail.com>
2026-02-23 14:25:54 +0800

    feat: throw custom not found error on non-existent messages

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,5 +1,9 @@
 import { messages } from '../db.js';
 
 export async function getMessages(req, res) {
+  if (!messages) {
+    throw new CustomNotFoundError('Messages not found')
+  }
+
   res.render("index", { title: "Mini Messageboard", messages: messages });
 };
--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -2,5 +2,10 @@ import { messages } from '../db.js';
 
 export async function getMessage(req, res) {
   const { index } = req.params
+
+  if (!messages[index]) {
+    throw new CustomNotFoundError('Message not found')
+  }
+
   res.render("message", { message: messages[index] });
 };
