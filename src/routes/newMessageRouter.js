f75c6edfa4f573126afdca24968132a4755fb8e8
RR <rrn091301@gmail.com>
2026-02-23 13:47:40 +0800

    feat: add form validation logic, pass values back to inputs, and show error messages on invalid inputs

--- src/routes/newMessageRouter.js
+++ src/routes/newMessageRouter.js
@@ -1,15 +1,44 @@
 import { Router } from 'express'
 import { messages } from '../db.js';
 
+import { body, validationResult, matchedData } from 'express-validator';
+
 const newMessageRouter = Router()
 
-newMessageRouter.get('/', (req, res) => {
-  res.render("form");
-})
-newMessageRouter.post('/', (req, res) => {
-  const messageText = req.body.messageText
-  const messageUser = req.body.messageUser
+// const alphaErr = "must only contain letters.";
+const messageUserMaxLength = 15;
+const messageTextMaxLength = 80;
+const userLengthErr = `must be between 1 and ${messageUserMaxLength} characters.`;
+const textLengthErr = `must be between 1 and ${messageTextMaxLength} characters.`;
 
+const validateUser = [
+  body("messageUser").trim()
+    .isLength({ min: 1, max: messageUserMaxLength }).withMessage(`Message user ${userLengthErr}`),
+  body("messageText").trim()
+    .isLength({ min: 1, max: messageTextMaxLength }).withMessage(`Message text ${textLengthErr}`),
+];
+
+newMessageRouter.get('/', (req, res) => {
+  res.render("form", {
+    values: {},
+    errors: {}
+  });
+});
+
+newMessageRouter.post('/', validateUser, (req, res) => {
+  const errors = validationResult(req);
+  const errorObj = {};
+    errors.array().forEach(err => {
+      errorObj[err.path] = err.msg;
+    });
+  const { messageText, messageUser } = matchedData(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).render("form", {
+      values: { messageText, messageUser },
+      errors: errorObj,
+    });
+  }
   messages.push({ text: messageText, user: messageUser, added: new Date() });
   res.redirect('/')
 })
